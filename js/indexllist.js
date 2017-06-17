75dd95cfbc66824eda59ca17b42478384494b359
xdlin424 <962202011@qq.com>
2017-06-18 00:05:14 +0800

    6.18首页下拉菜单

---
+++ js/indexllist.js
@@ -0,0 +1,137 @@
+$(function(){
+	$('#list ul .list1 .showa1').mouseover(function(){
+		$('#list ul .list1 .showbox1').show();
+		$('#list ul .list1 .showa1').css({"background":"url(img/index/images/indexListBg2H.jpg) no-repeat 184px 16px","background-color":"white","color":"#e84848","transition":"all 1s"});
+	});
+	$('#list ul .list1 .showa1').mouseout(function(){
+		$('#list ul .list1 .showbox1').hide();
+		$('#list ul .list1 .showa1').css({"background":"url(img/index/images/indexListBg2.jpg) no-repeat 184px 16px","background-color":"#e84848","color":"white"});
+	});
+	$('#list ul .list1 .showbox1').mouseover(function(){
+		$('#list ul .list1 .showbox1').show();
+		$('#list ul .list1 .showa1').css({"background":"url(img/index/images/indexListBg2H.jpg) no-repeat 184px 16px","background-color":"white","color":"#e84848","transition":"all 1s"});
+	});
+	$('#list ul .list1 .showbox1').mouseout(function(){
+		$('#list ul .list1 .showbox1').hide();
+		$('#list ul .list1 .showa1').css({"background":"url(img/index/images/indexListBg2.jpg) no-repeat 184px 16px","background-color":"#e84848","color":"white"});
+	});
+	
+	$('#list ul .list2 .showa1').mouseover(function(){
+		$('#list ul .list2 .showbox1').show();
+		$('#list ul .list2 .showa1').css({"background":"url(img/index/images/indexListBg2H.jpg) no-repeat 184px 16px","background-color":"white","color":"#e84848","transition":"all 1s"});
+	});
+	$('#list ul .list2 .showa1').mouseout(function(){
+		$('#list ul .list2 .showbox1').hide();
+		$('#list ul .list2 .showa1').css({"background":"url(img/index/images/indexListBg2.jpg) no-repeat 184px 16px","background-color":"#e84848","color":"white"});
+	});
+	$('#list ul .list2 .showbox1').mouseover(function(){
+		$('#list ul .list2 .showbox1').show();
+		$('#list ul .list2 .showa1').css({"background":"url(img/index/images/indexListBg2H.jpg) no-repeat 184px 16px","background-color":"white","color":"#e84848","transition":"all 1s"});
+	});
+	$('#list ul .list2 .showbox1').mouseout(function(){
+		$('#list ul .list2 .showbox1').hide();
+		$('#list ul .list2 .showa1').css({"background":"url(img/index/images/indexListBg2.jpg) no-repeat 184px 16px","background-color":"#e84848","color":"white"});
+	});
+	
+	$('#list ul .list3 .showa1').mouseover(function(){
+		$('#list ul .list3 .showbox1').show();
+		$('#list ul .list3 .showa1').css({"background":"url(img/index/images/indexListBg2H.jpg) no-repeat 184px 16px","background-color":"white","color":"#e84848","transition":"all 1s"});
+	});
+	$('#list ul .list3 .showa1').mouseout(function(){
+		$('#list ul .list3 .showbox1').hide();
+		$('#list ul .list3 .showa1').css({"background":"url(img/index/images/indexListBg2.jpg) no-repeat 184px 16px","background-color":"#e84848","color":"white"});
+	});
+	$('#list ul .list3 .showbox1').mouseover(function(){
+		$('#list ul .list3 .showbox1').show();
+		$('#list ul .list3 .showa1').css({"background":"url(img/index/images/indexListBg2H.jpg) no-repeat 184px 16px","background-color":"white","color":"#e84848","transition":"all 1s"});
+	});
+	$('#list ul .list3 .showbox1').mouseout(function(){
+		$('#list ul .list3 .showbox1').hide();
+		$('#list ul .list3 .showa1').css({"background":"url(img/index/images/indexListBg2.jpg) no-repeat 184px 16px","background-color":"#e84848","color":"white"});
+	});
+	
+	$('#list ul .list4 .showa1').mouseover(function(){
+		$('#list ul .list4 .showbox1').show();
+		$('#list ul .list4 .showa1').css({"background":"url(img/index/images/indexListBg2H.jpg) no-repeat 184px 16px","background-color":"white","color":"#e84848","transition":"all 1s"});
+	});
+	$('#list ul .list4 .showa1').mouseout(function(){
+		$('#list ul .list4 .showbox1').hide();
+		$('#list ul .list4 .showa1').css({"background":"url(img/index/images/indexListBg2.jpg) no-repeat 184px 16px","background-color":"#e84848","color":"white"});
+	});
+	$('#list ul .list4 .showbox1').mouseover(function(){
+		$('#list ul .list4 .showbox1').show();
+		$('#list ul .list4 .showa1').css({"background":"url(img/index/images/indexListBg2H.jpg) no-repeat 184px 16px","background-color":"white","color":"#e84848","transition":"all 1s"});
+	});
+	$('#list ul .list4 .showbox1').mouseout(function(){
+		$('#list ul .list4 .showbox1').hide();
+		$('#list ul .list4 .showa1').css({"background":"url(img/index/images/indexListBg2.jpg) no-repeat 184px 16px","background-color":"#e84848","color":"white"});
+	});
+	
+	$('#list ul .list5 .showa1').mouseover(function(){
+		$('#list ul .list5 .showbox1').show();
+		$('#list ul .list5 .showa1').css({"background":"url(img/index/images/indexListBg2H.jpg) no-repeat 184px 16px","background-color":"white","color":"#e84848","transition":"all 1s"});
+	});
+	$('#list ul .list5 .showa1').mouseout(function(){
+		$('#list ul .list5 .showbox1').hide();
+		$('#list ul .list5 .showa1').css({"background":"url(img/index/images/indexListBg2.jpg) no-repeat 184px 16px","background-color":"#e84848","color":"white"});
+	});
+	$('#list ul .list5 .showbox1').mouseover(function(){
+		$('#list ul .list5 .showbox1').show();
+		$('#list ul .list5 .showa1').css({"background":"url(img/index/images/indexListBg2H.jpg) no-repeat 184px 16px","background-color":"white","color":"#e84848","transition":"all 1s"});
+	});
+	$('#list ul .list5 .showbox1').mouseout(function(){
+		$('#list ul .list5 .showbox1').hide();
+		$('#list ul .list5 .showa1').css({"background":"url(img/index/images/indexListBg2.jpg) no-repeat 184px 16px","background-color":"#e84848","color":"white"});
+	});
+	
+	$('#list ul .list6 .showa1').mouseover(function(){
+		$('#list ul .list6 .showbox1').show();
+		$('#list ul .list6 .showa1').css({"background":"url(img/index/images/indexListBg2H.jpg) no-repeat 184px 16px","background-color":"white","color":"#e84848","transition":"all 1s"});
+	});
+	$('#list ul .list6 .showa1').mouseout(function(){
+		$('#list ul .list6 .showbox1').hide();
+		$('#list ul .list6 .showa1').css({"background":"url(img/index/images/indexListBg2.jpg) no-repeat 184px 16px","background-color":"#e84848","color":"white"});
+	});
+	$('#list ul .list6 .showbox1').mouseover(function(){
+		$('#list ul .list6 .showbox1').show();
+		$('#list ul .list6 .showa1').css({"background":"url(img/index/images/indexListBg2H.jpg) no-repeat 184px 16px","background-color":"white","color":"#e84848","transition":"all 1s"});
+	});
+	$('#list ul .list6 .showbox1').mouseout(function(){
+		$('#list ul .list6 .showbox1').hide();
+		$('#list ul .list6 .showa1').css({"background":"url(img/index/images/indexListBg2.jpg) no-repeat 184px 16px","background-color":"#e84848","color":"white"});
+	});
+	
+	$('#list ul .list7 .showa1').mouseover(function(){
+		$('#list ul .list7 .showbox1').show();
+		$('#list ul .list7 .showa1').css({"background":"url(img/index/images/indexListBg2H.jpg) no-repeat 184px 16px","background-color":"white","color":"#e84848","transition":"all 1s"});
+	});
+	$('#list ul .list7 .showa1').mouseout(function(){
+		$('#list ul .list7 .showbox1').hide();
+		$('#list ul .list7 .showa1').css({"background":"url(img/index/images/indexListBg2.jpg) no-repeat 184px 16px","background-color":"#e84848","color":"white"});
+	});
+	$('#list ul .list7 .showbox1').mouseover(function(){
+		$('#list ul .list7 .showbox1').show();
+		$('#list ul .list7 .showa1').css({"background":"url(img/index/images/indexListBg2H.jpg) no-repeat 184px 16px","background-color":"white","color":"#e84848","transition":"all 1s"});
+	});
+	$('#list ul .list7 .showbox1').mouseout(function(){
+		$('#list ul .list7 .showbox1').hide();
+		$('#list ul .list7 .showa1').css({"background":"url(img/index/images/indexListBg2.jpg) no-repeat 184px 16px","background-color":"#e84848","color":"white"});
+	});
+	
+	$('#list ul .list8 .showa1').mouseover(function(){
+		$('#list ul .list8 .showbox1').show();
+		$('#list ul .list8 .showa1').css({"background":"url(img/index/images/indexListBg2H.jpg) no-repeat 184px 16px","background-color":"white","color":"#e84848","transition":"all 1s"});
+	});
+	$('#list ul .list8 .showa1').mouseout(function(){
+		$('#list ul .list8 .showbox1').hide();
+		$('#list ul .list8 .showa1').css({"background":"url(img/index/images/indexListBg2.jpg) no-repeat 184px 16px","background-color":"#e84848","color":"white"});
+	});
+	$('#list ul .list8 .showbox1').mouseover(function(){
+		$('#list ul .list8 .showbox1').show();
+		$('#list ul .list8 .showa1').css({"background":"url(img/index/images/indexListBg2H.jpg) no-repeat 184px 16px","background-color":"white","color":"#e84848","transition":"all 1s"});
+	});
+	$('#list ul .list8 .showbox1').mouseout(function(){
+		$('#list ul .list8 .showbox1').hide();
+		$('#list ul .list8 .showa1').css({"background":"url(img/index/images/indexListBg2.jpg) no-repeat 184px 16px","background-color":"#e84848","color":"white"});
+	});
+})
